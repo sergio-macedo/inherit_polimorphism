85edf14533bf9b91bab5d4be47bff02fe670542e
sergio-macedo <sergio.garoma@hotmail.com>
2022-06-02 15:16:42 +0200

    using abstracts classes

--- src/course/Main.java
+++ src/course/Main.java
@@ -4,13 +4,36 @@ import entities.Account;
 import entities.BusinessAccount;
 import entities.SavingsAccount;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+
 public class Main {
 
     public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        List<Account> list = new ArrayList<>();
+        list.add(new SavingsAccount(1001, "Alex", 500.0, 0.01));
+        list.add(new BusinessAccount(1002,"Maria",1000.0,400.0));
+        list.add(new SavingsAccount(1003, "bob", 300.0, 0.01 ));
+        list.add(new BusinessAccount(1004,"Ana",500.0,500.0));
 
-        Account acc = new Account(1001,"Alex", 0.0);
-        BusinessAccount bacc = new BusinessAccount(1002,"Maria",0.0,500.00);
+        double sum = 0.0;
+        for (Account acc : list) {
+            sum += acc.getBalance();
+        }
 
+        System.out.println("Total balance" + sum);
+
+        for (Account acc : list) {
+            acc.deposit(10.0);
+        }
+        for (Account acc : list) {
+            System.out.printf("update balance %d: %.2f%n ",acc.getNumber(), acc.getBalance());
+        }
+
+
+        /*
         //UPCASTING
         //totally normal to make a object from the "original" class receive the info from the extended class"
         Account acc1 = bacc;
@@ -44,5 +67,8 @@ public class Main {
         Account acc8 = new BusinessAccount(3,"bob",1000.0,500.0);
         acc8.withdraw(200.0);
         System.out.println(acc8.getBalance());
+    }
+
+         */
     }
 }
--- src/course/Polimorph.java
+++ src/course/Polimorph.java
@@ -4,15 +4,15 @@ import entities.Account;
 import entities.SavingsAccount;
 
 public class Polimorph {
-    public static void main(String[] args) {
-        Account x = new Account(1,"Alex", 100.00);
-        Account y = new SavingsAccount(2, "bob", 100.0,0.1);
+    public static void main(String[] args) {/*
+        //Account x = new Account(1,"Alex", 100.00);
+        //Account y = new SavingsAccount(2, "bob", 100.0,0.1);
 
         x.withdraw(50.0);
         y.withdraw(50.0);
         System.out.println(y.getBalance());
         System.out.println(x.getBalance());
-
+*/
     }
 
 }
